62baead02299fef280d376257892927526a9e40c
BgJw <oskarstalker@gmail.com>
2021-01-13 09:13:10 +0200

    add question about films

--- Learning_Udemy/js/script.js
+++ Learning_Udemy/js/script.js
@@ -7,3 +7,25 @@ const obj = {
 console.log(obj);
 
 let arr = ['plum.png', 'orange.jpg', 'apple.bmp', 'banan.png'];
+
+
+
+let numberOfFilms = prompt('Cколько фильмов вы уже просмотрели?');
+
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+let lastWatchedFilms = prompt('Last watched Movie?');
+let ratingFilms = prompt('what rating will you put on the film?');
+
+personalMovieDB.movies.lastWatchedFilms = ratingFilms;
+
+console.log(personalMovieDB);
